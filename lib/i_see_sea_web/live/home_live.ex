8843140a93f2c6b41cf147e6eabb38be6816c252
Ivan46464 <ivan464@abv.bg>
2024-09-18 21:37:50 +0300

    Kinda redi

--- lib/i_see_sea_web/live/home_live.ex
+++ lib/i_see_sea_web/live/home_live.ex
@@ -46,7 +46,9 @@ defmodule ISeeSeaWeb.HomeLive do
         create_report_toolbox_is_open: false,
         create_report_type: nil,
         sidebar_open: true,
-        form_data: %{username: nil}
+        form_data: %{username: nil},
+        stats_panel_is_open: false,
+        selected_filters: []
       )
 
     {:ok, new_socket}
@@ -55,7 +57,7 @@ defmodule ISeeSeaWeb.HomeLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="relative md:inline flex flex-col mt-2 mx-4 md:mx-28 w-full h-full">
+    <div class="relative md:inline flex flex-col mt-2 mx-4 md:mx-28 w-7/12 h-full">
       <div
         id="map"
         class="absolute flex items-center h-full w-full z-0 rounded-md shadow-md"
@@ -76,6 +78,11 @@ defmodule ISeeSeaWeb.HomeLive do
         <p>Sidebar content goes here.</p>
       </div> --%>
     </div>
+
+     <HomeComponents.stat_home supports_touch={@supports_touch} stats_panel_is_open={@stats_panel_is_open} selected_filters={@selected_filters} />
+
+
+
     """
   end
 
@@ -136,4 +143,41 @@ defmodule ISeeSeaWeb.HomeLive do
   def handle_event("user_selected_location", %{"latitude" => lat, "longitude" => lon}, socket) do
     {:noreply, assign(socket, :user_selected_location, %{lat: lat, lon: lon})}
   end
+  @impl true
+def handle_event("toggle_stats_panel", _params, socket) do
+  new_state = !socket.assigns.stats_panel_is_open
+  {:noreply, assign(socket, stats_panel_is_open: new_state)}
+end
+
+@impl true
+def handle_event("add_filter", %{"filter" => filter}, socket) do
+  selected_filters =
+    if filter in socket.assigns.selected_filters do
+      socket.assigns.selected_filters
+    else
+      [filter | socket.assigns.selected_filters]
+    end
+
+  {:noreply, assign(socket, selected_filters: selected_filters)}
+end
+
+@impl true
+def handle_event("remove_filter", %{"filter" => filter}, socket) do
+  selected_filters =
+    socket.assigns.selected_filters
+    |> Enum.reject(fn f -> f == filter end)
+
+  {:noreply, assign(socket, selected_filters: selected_filters)}
+end
+
+@impl true
+def handle_event("remove_filter", %{"filter" => filter}, socket) do
+  selected_filters =
+    socket.assigns.selected_filters
+    |> Enum.reject(fn f -> f == filter end)
+
+  {:noreply, assign(socket, selected_filters: selected_filters)}
+end
+
+
 end
